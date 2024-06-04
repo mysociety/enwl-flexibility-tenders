18c2b77bedc01163114ece2bba3b82a98db0f4d5
Zarino Zappia <mail@zarino.co.uk>
2024-06-04 13:47:47 +0100

    Checkboxes to show/hide datasets individually

--- static/js/main.js
+++ static/js/main.js
@@ -5,9 +5,8 @@ createApp({
     data() {
         return {
             map: null,
-            areas: null, // Leaflet.GeoJSON layer, for storing all areas
-            points: null, // Leaflet.GeoJSON layer, for storing points
-            points2: null, // TODO: Sort this out!!
+            dataLayers: {}, // dataset details and Leaflet layers, keyed by dataset ID
+            visibleLayers: null, // Leaflet.FeatureGroup
             colors: {
                 tender: "#FC832A",
                 member: "#FF0000",
@@ -32,6 +31,7 @@ createApp({
             var _this = this;
 
             _this.map = L.map(_this.$refs.map).setView([54.0934, -2.8948], 7);
+            _this.visibleLayers = L.featureGroup().addTo(toRaw(_this.map));
 
             L.tileLayer(
                 'https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=7ac28b44c7414ced98cd4388437c718d',
@@ -46,7 +46,7 @@ createApp({
                 dataType: 'json',
                 url: 'static/js/areas.geojson'
             }).done(function(geojsonObj){
-                _this.areas = L.geoJSON(
+                var layer = L.geoJSON(
                     geojsonObj.features,
                     {
                         style: {
@@ -74,8 +74,19 @@ createApp({
                             });
                         }
                     }
-                ).addTo(toRaw(_this.map));
-                toRaw(_this.map).fitBounds(_this.areas.getBounds());
+                );
+
+                _this.dataLayers['areas'] = {
+                    id: 'areas',
+                    label: 'ENWL tender areas',
+                    layer: layer
+                };
+
+                _this.showDataLayer('areas');
+
+                toRaw(_this.map).fitBounds(
+                    _this.dataLayers['areas'].layer.getBounds()
+                );
             });
 
             $.ajax({
@@ -83,25 +94,36 @@ createApp({
                 dataType: 'json',
                 url: 'static/js/points.geojson'
             }).done(function(geojsonObj){
-                _this.points = L.geoJSON(
-                    geojsonObj.features,
-                    {
-                        pointToLayer: function(feature, latlng){
-                            return L.circleMarker(latlng, {
-                                radius: 4,
-                                color: _this.colors[feature.properties.category] || "#999"
-                            });
-                        },
-                        onEachFeature: function(feature, layer){
-                            layer.on('click', function(e){
-                                console.log(feature.properties);
-                            });
-                            layer.bindTooltip(feature.properties.category, {
-                                className: "pe-none" // prevent flicker when mousing over tooltip
-                            });
-                        }
+                _.each(
+                    _.groupBy(geojsonObj.features, function(feature){ return feature.properties.category; }),
+                    function(group, category){
+                        var layer = L.geoJSON(
+                            group,
+                            {
+                                pointToLayer: function(feature, latlng){
+                                    return L.circleMarker(latlng, {
+                                        radius: 4,
+                                        color: _this.colors[feature.properties.category] || "#999"
+                                    });
+                                },
+                                onEachFeature: function(feature, layer){
+                                    layer.on('click', function(e){
+                                        console.log(feature.properties);
+                                    });
+                                    layer.bindTooltip(feature.properties.category, {
+                                        className: "pe-none" // prevent flicker when mousing over tooltip
+                                    });
+                                }
+                            }
+                        );
+
+                        _this.dataLayers[category] = {
+                            id: category,
+                            label: 'Carbon Co-op: ' + category,
+                            layer: layer
+                        };
                     }
-                ).addTo(toRaw(_this.map));
+                );
             });
 
             $.ajax({
@@ -109,23 +131,24 @@ createApp({
                 dataType: 'json',
                 url: 'static/js/smart-meter-installation.geojson'
             }).done(function(geojsonObj){
-                _this.points2 = L.geoJSON(
+                var getColor = function(percentage) {
+                    if ( percentage < 20 ) {
+                        return '#ff0000'; // red
+                    } else if ( percentage < 40 ) {
+                        return '#ffc100'; // orange
+                    } else if ( percentage < 60 ) {
+                        return '#ffff00'; // yellow
+                    } else if ( percentage < 80 ) {
+                        return '#d6ff00'; // lime
+                    } else {
+                        return '#63ff00'; // green
+                    }
+                };
+
+                var layer = L.geoJSON(
                     geojsonObj.features,
                     {
                         pointToLayer: function(feature, latlng){
-                            var getColor = function(percentage) {
-                                if ( percentage < 20 ) {
-                                    return '#ff0000'; // red
-                                } else if ( percentage < 40 ) {
-                                    return '#ffc100'; // orange
-                                } else if ( percentage < 60 ) {
-                                    return '#ffff00'; // yellow
-                                } else if ( percentage < 80 ) {
-                                    return '#d6ff00'; // lime
-                                } else {
-                                    return '#63ff00'; // green
-                                }
-                            };
                             return L.polyMarker(latlng, {
                                 marker: "D",
                                 radius: 6,
@@ -144,8 +167,38 @@ createApp({
                             });
                         }
                     }
-                ).addTo(toRaw(_this.map));
+                );
+
+                _this.dataLayers['smart-meters'] = {
+                    id: 'smart-meters',
+                    label: 'ENWL smart meter adoption rate',
+                    layer: layer
+                };
             });
+        },
+
+        hideDataLayer(id){
+            var _this = this;
+            _this.dataLayers[id].layer.removeFrom(_this.visibleLayers);
+        },
+
+        showDataLayer(id){
+            var _this = this;
+            _this.dataLayers[id].layer.addTo(_this.visibleLayers);
+        },
+
+        dataLayerIsVisible(id){
+            var _this = this;
+            return _this.visibleLayers.hasLayer( _this.dataLayers[id].layer );
+        },
+
+        toggleDataLayer(id){
+            var _this = this;
+            if ( _this.dataLayerIsVisible(id) ) {
+                _this.hideDataLayer(id);
+            } else {
+                _this.showDataLayer(id);
+            }
         }
     }
 }).mount('#app');
